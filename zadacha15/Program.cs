969c7e783b4b31a9056b3d3b3f5afe1d7831bb8d
lavrenoff <lavr020775@gmail.com>
2022-08-25 22:05:37 +0300

    zadacha15

--- zadacha15/Program.cs
+++ zadacha15/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.Write("Введите число от 1 до 7: ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber >= 1 && dayNumber <= 7)
+    {
+        if (dayNumber == 7 || dayNumber == 6)
+        {
+            Console.Write("Под цифрой " + dayNumber + " - Выходной день");
+        }
+        else
+        {
+            Console.Write("Под цифрой " + dayNumber + " - Рабочий день");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+    }
+    
+
